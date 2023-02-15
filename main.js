9641213c9c5265ac0fc0bf92efeab2dd0c059d32
HujjatGh <hujjat.pg@gmail.com>
2023-02-15 20:42:33 +0100

    csde

---
+++ main.js
@@ -0,0 +1,7 @@
+// Task 4
+let newParagraph = document.createElement('p');
+newParagraph.innerText = "Hello World!";
+
+const btnText = document.querySelector("#");
+const para = document.querySelector(#article)
+btnText.addEventListener("click", function(){})
